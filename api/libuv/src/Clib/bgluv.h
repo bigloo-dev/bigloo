9a3e2bb3153e6309d51a35de670cbae37b0dcc51
Marc Feeley <feeley@iro.umontreal.ca>
2018-08-16 06:11:54 -0400

    16Aug18

--- api/libuv/src/Clib/bgluv.h
+++ api/libuv/src/Clib/bgluv.h
@@ -1,9 +1,9 @@
 /*===========================================================================*/
 /*   (Llib/uvtypes.scm)                                                      */
 /*   Bigloo (4.3b)                                                           */
-/*   Inria -- Sophia Antipolis (c)       Wed Feb 14 20:13:30 CET 2018        */
+/*   Inria -- Sophia Antipolis (c)       Thu Aug 16 05:58:41 EDT 2018        */
 /*===========================================================================*/
-/* COMPILATION: (/home/serrano/prgm/project/bigloo/bigloo/bin/bigloo -O3 -fcfa-arithmetic -q -lib-dir /home/serrano/prgm/project/bigloo/bigloo/lib/bigloo/4.3b -I Llib -lib-dir /home/serrano/prgm/project/bigloo/bigloo/lib/bigloo/4.3b -srfi libuv -copt -I/home/serrano/prgm/project/bigloo/bigloo/libuv/libuv-1.x-24jan2015/include -safee -unsafe -copt -fPIC -copt -IClib -copt  Llib/uvtypes.scm -o Clib/bgluv.h -hgen) */
+/* COMPILATION: (/home/feeley/onpush-daemon/repos/git@github.com/bigloo-dev/bigloo/bin/bigloo -O3 -fcfa-arithmetic -q -lib-dir /home/feeley/onpush-daemon/repos/git@github.com/bigloo-dev/bigloo/lib/bigloo/4.3b -I Llib -lib-dir /home/feeley/onpush-daemon/repos/git@github.com/bigloo-dev/bigloo/lib/bigloo/4.3b -srfi libuv -copt -I/home/feeley/onpush-daemon/repos/git@github.com/bigloo-dev/bigloo/libuv/libuv-v1.20.3/include -safee -unsafe -copt -fPIC -copt -IClib -copt  Llib/uvtypes.scm -o Clib/bgluv.h -hgen) */
 
 /* object type definitions */
 typedef struct BgL_z52uvz52_bgl {
